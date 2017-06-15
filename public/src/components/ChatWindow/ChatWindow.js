a85557626de6efe48de4b56fa97a9bd8f3bd4192
= <webdev.jameslemire@gmail.com>
2017-06-15 12:32:55 -0600

    JL solution - Test.

--- public/src/components/ChatWindow/ChatWindow.js
+++ public/src/components/ChatWindow/ChatWindow.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import './ChatWindow.css';
 
-export default function ChatWindow() {
-  return (
-    <div id="ChatWindow__container">
-      <div id="ChatWindow__messagesContainer">
+export default class ChatWindow extends Component {
+  render() {
+    return (
+      <div id="ChatWindow__container">
+        <div id="ChatWindow__messagesContainer">
 
+        </div>
+        <div id="ChatWindow__newMessageContainer">
+          <input placeholder="What's on your mind? Press enter to send." />
+        </div>
       </div>
-      <div id="ChatWindow__newMessageContainer">
-        <input placeholder="What's on your mind? Press enter to send." />
-      </div>
-    </div>
-  )
+    )
+  }
 }
