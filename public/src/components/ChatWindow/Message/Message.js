b34958a5b10be4093a926c6a145e3fa91f49af48
= <webdev.jameslemire@gmail.com>
2017-06-15 13:49:59 -0600

    JL solution - Front-end can Create, Read, and Delete

--- public/src/components/ChatWindow/Message/Message.js
+++ public/src/components/ChatWindow/Message/Message.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import './Message.css';
+
+import FaTrash from "react-icons/lib/fa/trash";
+import FaPencil from "react-icons/lib/fa/pencil";
 
 export default class Message extends Component {
   constructor(props) {
@@ -9,9 +13,13 @@ export default class Message extends Component {
   }
 
   render() {
+    const { id, text, time, edit, remove } = this.props;
     return (
-      <div>
-        <p> Message Here </p>
+      <div className="Message__container">
+        <span className="Message__time">{time}</span>
+        <span className="Message__text">{text}</span>
+        <span className="Message__edit"> <FaPencil /> </span>
+        <span className="Message__delete" onClick={ () => remove( id ) }> <FaTrash /> </span>
       </div>
     )
   }
--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -1,10 +1,13 @@
-let messages = [];
-let id = 0;
+let messages = [ { id: 1, text: 'Oh hey!', time: '06/15/2017 - 1:26 pm' },
+                 { id: 0, text: 'Hey dude!', time: '06/15/2017 - 1:25 pm' }
+               ];
+let id = 2;
 
 module.exports = {
   create: ( req, res ) => {
-    const { message } = req.body;
-    messages.push({ id, message });
+    console.log('Create hit:', req.body);
+    const { text, time } = req.body;
+    messages.unshift({ id, text, time });
     id++;
     res.status(200).send( messages );
   },
@@ -14,12 +17,13 @@ module.exports = {
   },
 
   update: ( req, res ) => {
-    const { message } = req.body;
+    const { text } = req.body;
     const updateID = req.params.id;
 
     messages[ updateID ] = {
       id: messages[ updateID ].id,
-      message: message || messages[ updateID ].message
+      text: text || messages[ updateID ].message,
+      time: messages[ updateID ].time
     };
 
     res.status(200).send( messages );
